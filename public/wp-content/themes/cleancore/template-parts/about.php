9a8ce4478affffddd715e3fa7c5661f53d1dc1cf
Guy-Argaman <guy@mouseux.com>
2025-11-09 18:03:29 +0200

    Added Polylang + Pages + Glassglow test - needs work, need to add videos compatible with background and responsive.

--- public/wp-content/themes/cleancore/template-parts/about.php
+++ public/wp-content/themes/cleancore/template-parts/about.php
@@ -9,10 +9,16 @@ if ( ! defined( 'ABSPATH' ) ) {
         <div class="about-circle"></div>
         <div class="about--wrapper">
             <div class="glassbox">
-                <h1>Shaham Tech</h1>
-                <h2><?php echo get_the_title();?></h2>
-                <div class="about-text">
-                    <?php echo apply_filters('the_content',get_the_content());?>
+                <div class="glassbow-content">
+                    <h1>Shaham Tech</h1>
+                    <h2><?php echo get_the_title();?></h2>
+                    <div class="about-text">
+                        <?php echo apply_filters('the_content',get_the_content());?>
+                    </div>
+                </div>
+                <div class="glassbox-videos">
+                    
+
                 </div>
             </div>
         </div>
