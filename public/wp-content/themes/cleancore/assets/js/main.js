abd2d7749028aecea806d21c00cd627b7ef6ddc5
Guy <guy@mouseux.com>
2025-11-12 13:26:42 +0200

    Fixed Tiles Respnosiveness in About Hero Section, Adjusted Overall design and Logic

--- public/wp-content/themes/cleancore/assets/js/main.js
+++ public/wp-content/themes/cleancore/assets/js/main.js
@@ -27,7 +27,31 @@ jQuery(document).ready(function ($) {
 
     build();
 
-    const width = tiles.width();
-    const height = tiles.height();
-    console.log(width, height)
+    resizeElement('.glassbox', $('.tile').height() * 3, $('.tile').width() * 6);
+    resizeVideos();
+
+    function resizeElement(target, height, width) {
+        const elements = $(target);
+        elements.css({ height: `${height}px`, width: `${width}px` });
+    }
+
+    function resizeVideos() {
+        const width = $('.tile').width();
+        const height = $('.tile').height();
+
+        $('.glassbox .glassbox-videos .image-wrapper').each(function (i, el) {
+            resizeElement(el, height, width);
+        });
+    }
+
+    $(window).on('resize', function () {
+        const width = $('.tile').width();
+        const height = $('.tile').height();
+
+        resizeElement('.glassbox', height * 3, width * 6);
+        resizeVideos();
+    });
+
+
+
 });
